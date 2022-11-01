b0a624fea4fc4fed1720eb64a9160930b874da9f
Katherine <73628642+KatherineAWard@users.noreply.github.com>
2022-11-01 14:59:18 -0700

    Add basic login/register

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Project1
+{
+    public static string username = "username";
+    public static string password = "password";
+
+    public static void Main()
+    {
+        Console.WriteLine("Do you have an account?");
+        if (Console.ReadLine().Equals("yes"))
+        {
+            Login();
+        }
+        else
+        {
+            Register();
+        }
+    }
+
+    public static void Login()
+    {
+        string userTry;
+        string passTry;
+        while (true)
+        {
+            Console.WriteLine("Enter username:");
+            userTry = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            passTry = Console.ReadLine();
+
+            if (userTry == username && passTry == password)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Username or Password were incorrect.");
+                continue;
+            }
+        }
+        Console.WriteLine("Successfully logged in.");
+    }
+
+
+    public static void Register()
+    {
+        Console.WriteLine("Enter a username:");
+        username = Console.ReadLine();
+        Console.WriteLine("Enter a password:");
+        password = Console.ReadLine();
+        Console.WriteLine("Successfully registered.");
+    }
+
+}
