2fee48454c43a902f42fae57dc9cdafaecf9f73b
Katherine <73628642+KatherineAWard@users.noreply.github.com>
2022-11-02 15:20:37 -0700

    Begin console menu system

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -7,20 +7,42 @@ namespace Project1
         public static void Main()
         {
             LoginInfo loginInfo = new LoginInfo();
+            bool isManager = false;
 
+            Console.WriteLine("What would you like to do?\n1) Login as Employee\n2) Login as Manager\n3) Register");
+            int option;
             while (true)
             {
-                Console.WriteLine("Do you have an account?");
-                if (Console.ReadLine().Equals("yes"))
+                if (!(Int32.TryParse(Console.ReadLine(), out option)) || option < 1 || option > 3)
+                {
+                    Console.WriteLine("Please enter '1' or '2'");
+                }
+                else if (option == 1)
                 {
                     loginInfo.Login();
+                    break;
                 }
-                else
+                else if (option == 2)
+                {
+                    loginInfo.Login();
+                    isManager = true;
+                    break;
+                }
+                else if (option == 3)
                 {
                     loginInfo.Register();
+                    break;
                 }
             }
 
+            if (!isManager)
+            {
+                Console.WriteLine("What would you like to do?\n1) Submit Ticket\n2) View Previous Tickets");
+            }
+            else
+            {
+                Console.WriteLine("What would you like to do?\n1) View Tickets\n2) View Previous Tickets");
+            }
         }
     }
 }
